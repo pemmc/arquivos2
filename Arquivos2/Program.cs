10cbdbea2117dc0125487ef9f8292bfa5564dfb8
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-20 11:01:38 -0300

    Usando BLOCO using... e instanciando o StreamReader apenas com o File.OpenText

--- Arquivos2/Program.cs
+++ Arquivos2/Program.cs
@@ -16,6 +16,7 @@ namespace Arquivos2
             //mac
             // diretorio padrao '/Users/nxgames/Arquivos2/Arquivos2/Projeto1/bin/Debug/netcoreapp3.1/arquivotexto.txt
 
+            /* VERSAO 1 sem o BLOCO USING
             string path = "/Users/nxgames/Projects/Arquivos2/Arquivos2/arquivotexto.txt";
 
 
@@ -54,6 +55,59 @@ namespace Arquivos2
 
                 if (sr != null) sr.Close();
                 //if (fs != null) fs.Close();
+
+            }
+            */
+
+            /*
+            //versao com o BLOCO USING
+            string path = "/Users/nxgames/Projects/Arquivos2/Arquivos2/arquivotexto.txt";
+
+            try
+            {
+                //tudo estanciado neste bloco USING e ao final ele será fechado
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERRO (IO): " + e.Message);
+
+            }
+            */
+
+            //versao com o BLOCO USING... estanciando o StreamReader de forma resumida, apenas com a classe File
+            string path = "/Users/nxgames/Projects/Arquivos2/Arquivos2/arquivotexto.txt";
+
+            try
+            {
+                //tudo estanciado neste bloco USING e ao final ele será fechado
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+
+                        Console.WriteLine(line);
+
+                    }
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERRO (IO): " + e.Message);
 
             }
         }
